9d7ed16adf3b51426d224bfe56e644582d2f7739
Michal Czyzowicz <czyzowicz.m@gmail.com>
2018-07-25 19:40:08 +0200

    commit

--- src/main/java/fantasy/controller/UserController.java
+++ src/main/java/fantasy/controller/UserController.java
@@ -2,7 +2,6 @@ package fantasy.controller;
 
 import fantasy.model.User;
 import fantasy.service.UserService;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
