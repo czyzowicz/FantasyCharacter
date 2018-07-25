3756ccdd5ff708049e1479a8f7b675b854327b47
Artur Kowalski <a.kowalski@ariesrk.pl>
2018-07-25 17:41:34 +0200

    comitXx

---
+++ src/main/java/fantasy/service/impl/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package fantasy.service.impl;
+
+import fantasy.model.User;
+import fantasy.repository.UserRepository;
+import fantasy.service.UserService;
+
+import java.util.Set;
+
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public void create(String email, String password) {
+        User user = new User();
+        user.setMail(email);
+        user.setPassword(password);
+        userRepository.save(user);
+
+
+    }
+
+    @Override
+    public Set<User> findAll(String email) {
+        return userRepository.findOneByEmail(email);
+    }
+}
